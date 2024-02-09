7725c1d5c1fc8d2bfffeed1bb29265aeb751236a
account_name <dlongminhtan@gmail.com>
2024-02-09 23:49:20 +0700

    okiii

---
+++ assets/js/animation.js
@@ -0,0 +1,47 @@
+var napm = document.querySelector(".lx__nap")
+var napd = document.querySelector(".lx__container--nap")
+var opened = document.querySelector(".lx__container--open")
+var lx_ctn = document.querySelector(".lx__container")
+var lx_write = document.querySelector(".lx_write")
+var lx_form = document.querySelector(".lx_form")
+
+opened.addEventListener('click', () => {
+    opened.style.display = "none";
+    napd.style.height = "0px";
+    setTimeout(() => {
+        napm.style.height = "60px";
+    }, 500);
+    setTimeout(() => {
+        lx_write.style.transform = "translateY(-400px)"
+        lx_write.style.height = "400px";
+    }, 1000);
+    setTimeout(() => {
+        lx_write.style.transform = "translateY(-1000px)"
+    }, 1500);
+    setTimeout(() => {
+        lx_write.style.transform = "translateY(0px)"
+        lx_write.style.height = "630px"
+        lx_write.style.margin = "70px 0 0 5%"
+        lx_write.classList.add('br12px')
+        lx_form.style.display = "flex"
+    }, 2000)
+})
+
+document.addEventListener('click', function (e) {
+    // Kiểm tra xem click có xảy ra ngoài khu vực lx_ctn không
+    if (!lx_ctn.contains(e.target) && !opened.contains(e.target)) {
+        // Đặt lại các thuộc tính style để "đóng" thiệp
+        opened.style.display = "";
+        napd.style.height = "";
+        lx_write.style.transform = "";
+        lx_write.style.height = "";
+        lx_write.style.margin = "";
+        lx_write.classList.remove('br12px');
+        lx_form.style.display = "none";
+
+        // Đặt lại chiều cao của napm sau khi "đóng" thiệp
+        setTimeout(() => {
+            napm.style.height = "0px";
+        }, 500);
+    }
+});
